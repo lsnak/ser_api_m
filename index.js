acad951ef21ef8f5e2953a9169aaee9192b56d22
lsnak <rentlycur36@gmail.com>
2025-08-07 03:17:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.options('*', cors());
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMembers]
