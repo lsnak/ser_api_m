c52aded664baeb28c3f65ac5bf279aed7ba446c3
lsnak <rentlycur36@gmail.com>
2025-08-07 02:36:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.get('/server/:guildId', async (req, res) => {
   }
 });
 
+app.get('/health', (req, res) => res.send('OK'));
+
 app.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}`);
 });
